a962a880954a905a4d753ac32648684b5e4287d2
babloo-Chauhan <b8416800849@gmail.com>
2026-01-16 15:18:10 +0530

    gh

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,15 +1,14 @@
 import dotenv from "dotenv";
-dotenv.config(); 
+dotenv.config();
 
 import mysql from "mysql2/promise";
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  port: Number(process.env.DB_PORT),
 });
-
 
 export default db;

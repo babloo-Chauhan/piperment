6b540303faf3fe9dbc9a081b18322c1c6ceaa5c9
babloo-Chauhan <b8416800849@gmail.com>
2026-01-16 13:53:39 +0530

    cors

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://piperment.onrender.com//api",
+  baseURL: "https://piperment.onrender.com/api",
 });
 
 api.interceptors.request.use((req) => {
